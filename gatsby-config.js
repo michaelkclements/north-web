ef849acf1140e8571c3a9b6dabfc617eb8dcb6fd
michaelkclements <michaelkc@me.com>
2021-01-21 20:31:54 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "North",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+  ],
+};
